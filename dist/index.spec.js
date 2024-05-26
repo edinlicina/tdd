07c4ef40da3b3100b3e354c17a5a20740b72ef52
Edin <edin.licina.2@gmail.com>
2024-05-26 17:54:41 +0200

    configured test framework

---
+++ dist/index.spec.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const globals_1 = require("@jest/globals");
+(0, globals_1.test)("hello world", () => { });
+//# sourceMappingURL=index.spec.js.map
